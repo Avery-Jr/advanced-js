08c7a8261b0c823f6dd88d0d82a60fab6d90ba1f
Avery-Jr <averys34@nycstudents.net>
2025-02-03 14:53:48 +0000

    practice 2

--- LAB1/practice/script.js
+++ LAB1/practice/script.js
@@ -18,8 +18,21 @@ alert(typeof 6.4);
 
 // Let vs Const vs Var
 
+var x = 15;
+
+let age = 20;
+age = 10;
+
+const z = "Hey there";
+z = "Here's johnny";
+
 // Reassignment (no let)
 
+age = 15;
+age = 25;
+age = 35;
+
+alert(age);
 
 // Essential Operations
 
